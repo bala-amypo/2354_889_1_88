5197439883c720c063c533dc890cf73325c9f977
bala-amypo <bala@amypo.in>
2025-12-18 10:03:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApartmentUnitRepo.java
+++ src/main/java/com/example/demo/repository/ApartmentUnitRepo.java
@@ -1,3 +1,7 @@
 package com.example.demo.repository;
 
-import org.springframework
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.ApartmentUnit;
+public interface DeliveryEvaluationRepository extends JpaRepository<DeliveryEvaluation,Long>{
+    
+}
