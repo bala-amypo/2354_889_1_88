9b3baa56207ca6d193ba2f950d16a8bcd1a9534a
bala-amypo <bala@amypo.in>
2025-12-18 15:37:25 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,15 +1,21 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank
     private String name;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @Size(min = 6)
     private String password;
 }
